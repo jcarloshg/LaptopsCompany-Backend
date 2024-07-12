a846f0d9dfc026612ed69067589bfee170aa07aa
jcarloshg <carlosj12336@gmail.com>
2024-07-11 23:23:25 -0600

    first commit

---
+++ src/main/java/com/example/laptopcompany/LaptopcompanyApplication.java
@@ -0,0 +1,13 @@
+package com.example.laptopcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopcompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopcompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/laptopcompany/LaptopcompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.laptopcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopcompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
